ff535b0a23c1f41f5205ddef06f12cc3cfad327e
Maksym Kultyshev <mksbnt@icloud.com>
2023-10-09 00:20:54 +0300

    upd: write coments

--- src/services/frame_service.py
+++ src/services/frame_service.py
@@ -3,6 +3,11 @@ import numpy as np
 
 
 def process_frame(frame, templates, threshold):
+    """
+    This function processes a single frame of a video, looking for objects in the frame.
+    If an object is found, a rectangle is drawn around it and the word "Object" is written
+    above the rectangle.
+    """
     img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     for template in templates:
@@ -15,11 +20,12 @@ def process_frame(frame, templates, threshold):
 
             # Draw a rectangle around the match
             cv2.rectangle(
-                frame, pt, (pt[0] + template.shape[1], pt[1] + template.shape[0]), (0, 0, 255), 2)
+                frame, pt, (pt[0] + template.shape[1], pt[1] + template.shape[0]), (0, 0, 255), 3)
 
-            cv2.putText(frame, 'Object', (pt[0], pt[1]-6), cv2.FONT_HERSHEY_PLAIN,
+            # Write "Object" on top of the rectangle
+            cv2.putText(frame, 'Object', (pt[0], pt[1]-8), cv2.FONT_HERSHEY_PLAIN,
                         2, (0, 0, 255), 2, cv2.LINE_AA)
 
-            break  # try with more than one object
+            break  # try with more than one object to identify
 
     return frame
